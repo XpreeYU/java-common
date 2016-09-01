4fd5f533054a2bd7e1f6684a6a14a4d6331fd536
yujiansong <yujianson@163.com>
2016-09-01 09:12:34 +0800

    commit json

---
+++ src/main/java/util/JsonUtil.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import com.alibaba.fastjson.JSON;
+public class JsonUtil{
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	public static <T>T convertJson2Object(String content, Class<T> valueType){
+		if(content == null || content.equals("")) 
+			return null;
+		return JSON.parseObject(content, valueType);
+	}
+	
+	public static String convertObject2Json(Object obj) {
+		return JSON.toJSONString(obj, true);
+	}
+	
+	public static Map<String, Object> convertJson2Map(String content){
+		try {
+			return mapper.readValue(content,  new TypeReference<Map<String,Object>>() { });
+		} catch (IOException e) {
+		}
+		return null;
+	}
+}
